0d553ef24747962a18b707dd50aa732fbd985eb1
Saurabh Singh <saurabh.rgt@gmail.com>
2023-04-15 10:08:52 +0530

    15th-april

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h1>User : {name}</h1>
+      <div className="chat-container">
+        <p className="chatbox">
+          <strong> name: </strong>
+          <span>chat message</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
